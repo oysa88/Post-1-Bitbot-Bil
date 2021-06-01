2ae0fed21a5feb365aadde59826d19b7cd1d0f09
oysa88 <oystein.salomonsen@tekniskmuseum.no>
2021-06-01 17:31:53 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,75 @@
+function RiktigLøsning () {
+    bitbot.setLedColor(0x00FF00)
+    bitbot.rotatems(BBRobotDirection.Left, 100, 4000)
+    Restart()
+}
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 10) {
+        LøstOppgave = true
+        RiktigLøsning()
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    Restart()
+})
+function Restart () {
+    LøstOppgave = false
+    Sekvens = false
+    bitbot.setLedColor(0xFFFF00)
+}
+function FeilLøsning () {
+    bitbot.setLedColor(0xFF0000)
+    bitbot.goms(BBDirection.Forward, 0, 4000)
+    Restart()
+}
+radio.onReceivedValue(function (name, value) {
+    if (name == "P") {
+        Pitch = value * -20
+    } else if (name == "A") {
+        Arm = value
+    } else if (name == "R") {
+        Roll = value * -20
+    }
+    if (!(Sekvens)) {
+        Sekvens = true
+        Kjøretid = input.runningTime()
+    }
+})
+let DriveRight = 0
+let DriveLeft = 0
+let Kjøretid = 0
+let Roll = 0
+let Arm = 0
+let Pitch = 0
+let Sekvens = false
+let LøstOppgave = false
+bitbot.select_model(BBModel.XL)
+radio.setGroup(1)
+let Sekvenstid = 4000
+Restart()
+basic.forever(function () {
+    if (Arm == 1) {
+        DriveLeft = Pitch - pins.map(
+        Roll,
+        0,
+        1023,
+        Pitch,
+        Pitch * -1
+        )
+        DriveRight = Pitch - pins.map(
+        Roll,
+        0,
+        -1023,
+        Pitch,
+        Pitch * -1
+        )
+        bitbot.motor(BBMotor.Left, Pitch - DriveLeft)
+        bitbot.motor(BBMotor.Right, Pitch - DriveRight)
+    } else {
+        bitbot.motor(BBMotor.Both, 0)
+    }
+    if (input.runningTime() - Kjøretid == Sekvenstid && !(LøstOppgave)) {
+        radio.sendNumber(11)
+        FeilLøsning()
+    }
+})
